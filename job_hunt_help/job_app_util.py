914e9aefa76e7e67806a4b3c8f9ba6406b3b93d2
Arjun Gupta <arjungupta@Arjuns-Air.fios-router.home>
2018-11-13 19:04:57 -0500

    further clean up, this is not bad at all

--- job_hunt_help/job_app_util.py
+++ job_hunt_help/job_app_util.py
@@ -1,10 +1,8 @@
 ## want to write a utility that allows us to generate our cover letters and our cold emails programmatically, so we minimize the chance of human error
-def coldEmailWriter(email_text):
-	final_email_text = email_text % (name, company, position, company, company)
-	print(final_email_text)
 
-def coldEmailWriterAlt(email_text_2):
-	print(email_text_2)
+def coldEmailWriter(email_text):
+	print('\n\n')
+	print(email_text)
 
 
 def getPositionsString(positions_list):
@@ -29,12 +27,14 @@ position_string = getPositionsString(positions_list)
 company = 'Capsule'
 
 #email_text = "Hi %s, My name is Arjun, and I am a 2017 Wharton graduate with a keen interest in analytics.\nI came across a few data-focused positions at %s this morning (%s).\nI believe these positions at %s would provide me with an outstanding opportunity to learn and help %s succeed at the same time."
+piece1 = 'Hi '
+piece2 = ',\n\nMy name is Arjun, and I am a 2017 Wharton graduate with a keen interest in analytics.\n\nI came across '
+piece3 = ' at '
+piece4 = ' this morning ('
+piece5 = "). These positions are very exciting to me, and I believe that my skills align with your requirements.\n\nFor some background, I've spent the last year and a half working as a data analyst focused on data quality at Coatue Asset Management, a long-short hedge fund. Despite the fact that I've gotten a lot out of this experience, I believe working at "
+piece6 = ' would provide me with an outstanding opportunity to learn and help '
+piece7 = ' succeed at the same time.\n\nI found your contact in Penn’s alumni database and was hoping that you could possibly direct me to a general recruiter or the recruiter specifically for this role. My resume is attached to this email. I would be extremely grateful for any help or contact regarding this opportunity!\nThank you in advance for your time!\n\nCheers,\n\nArjun\nhttps://www.linkedin.com/in/arjun-s-gupta-193a178a/' 
 
+email_text = piece1 + name + piece2 + quantity_of_roles_language + piece3 + company + piece4 + position_string + piece5 + company + piece6 + company + piece7
 
-email_text_2 = 'Hi ' + name + ',\nMy name is Arjun, and I am a 2017 Wharton graduate with a keen interest in analytics.\nI came across ' + quantity_of_roles_language + ' at '  + company + ' this morning (' + position_string + ').\nI believe working at ' + company + ' would provide me with an outstanding opportunity to learn and help ' + company + ' succeed at the same time.' 
-
-
-
-#coldEmailWriter(email_text)
-print('\n\n')
-coldEmailWriterAlt(email_text_2)
+coldEmailWriter(email_text)
