ffd320a967268501ca89a7707d53e948412cad3c
Arjun Gupta <31854693+arjungup740@users.noreply.github.com>
2020-02-17 12:50:09 -0500

    most recent version

--- job_hunt_help/test_email_runner.py
+++ job_hunt_help/test_email_runner.py
@@ -3,12 +3,23 @@ import smtplib
 from datetime import date
 import os 
 
+def sendEmail(sent_from, to, email_text, gmail_user, gmail_password):
+    
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print ('Email sent!')
+
 gmail_user = 'arjungup740@gmail.com'  
 gmail_password = 
 
 sent_from = gmail_user  
 to = ['arjungup740@gmail.com']
 subject = 'Daily Health Reminder %s' % date.today()
+# subject = 'Welcome Back!'
 # body = """
 # reminder to do something healthy today! Love you!
 # """
@@ -20,19 +31,30 @@ already_sent_folder = messages_folder + 'already_sent'
 ## list messages
 to_send_messages = os.listdir(to_send_folder)
 
-## check to see if there any messages available, otherwise make some new ones
 try:
 	indiv_message = to_send_messages[0]
-except Exception as e: ## if there aren't, then email a reminder to yourself to add some new ones
-	
+except: ## if there aren't, then email a reminder to yourself to add some new ones
+	print('got here')
+	subject = 'Auto-emailer out of messages'	
+	body = 'you know what to do'
+	email_text = """From: %s  
+	To: %s  
+	Subject: %s
+	%s
+	""" % (sent_from, ", ".join(to), subject, body)
 
+	sendEmail(sent_from, to, email_text, gmail_user, gmail_password)
+
+	print('got here 2')
+	sys.exit()
+
+## if there is a message there we proceed
 final_to_send_path = os.path.join(to_send_folder, indiv_message)
 final_replacement_path = os.path.join(already_sent_folder, indiv_message)
 
 with open(final_to_send_path, 'r') as file:
     body = file.read()#.replace('\n', '')
 
-# print(body)
 
 email_text = """From: %s  
 To: %s  
@@ -41,19 +63,11 @@ Subject: %s
 """ % (sent_from, ", ".join(to), subject, body)
 
 try:  
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
 
-    print ('Email sent!')
-    # print('moving this message to already sent folder')
-    os.rename(final_to_send_path, final_replacement_path)
-    # print(final_to_send_path)
-    # print(final_replacement_path)
+    sendEmail(sent_from, to, email_text, gmail_user, gmail_password)
+    os.rename(final_to_send_path, final_replacement_path) # move file from to send to already sent
     print('moved file')
-
+    jim
 
 except Exception as e:
     print ('Something went wrong' + str(e))
